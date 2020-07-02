2a1f6b0bacc1632a7bfc9b10dbe87c7bf372e39b
ALEXANDER POROKHIN (workplace) <softoad@yandex.ru>
2020-07-02 10:27:32 +0300

    mend

--- src/main/kotlin/ru/exrates/utils/ClientCodes.kt
+++ src/main/kotlin/ru/exrates/utils/ClientCodes.kt
@@ -1,7 +1,7 @@
 package ru.exrates.utils
 
 object ClientCodes {
-    const val SUCCESS = 200
+    const val SUCCESS = 700
     const val EXCHANGE_NOT_ACCESSIBLE = 701
     const val EXCHANGE_NOT_FOUND = 702
 }
