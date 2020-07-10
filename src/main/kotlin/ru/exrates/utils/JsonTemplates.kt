57a6cc8fdb01135ba1a76c5e86b4e4db080d07ad
ALEXANDER POROKHIN <softoad@yandex.ru>
2020-07-10 20:24:13 +0300

    fix curs period success code

--- src/main/kotlin/ru/exrates/utils/JsonTemplates.kt
+++ src/main/kotlin/ru/exrates/utils/JsonTemplates.kt
@@ -7,7 +7,7 @@ data class ExchangePayload(val exId: Int, val interval: String, val pairs: Array
  * @param interval common interval for all pairs
  * @param values map of curs where key is name of pair and value is price changing
  * */
-data class CursPeriod(val interval: String, val values: Map<String, Double>,  val status: Int = 200)
+data class CursPeriod(val interval: String, val values: Map<String, Double>,  val status: Int = ClientCodes.SUCCESS)
 
 class ErrorBody(val message: String)
 
