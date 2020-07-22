b1bbd17f9036bd9097a7623978a45801704ece0a
ALEXANDER POROKHIN <softoad@yandex.ru>
2020-07-22 18:08:35 +0300

    ws Exchange created

---
+++ src/main/kotlin/ru/exrates/entities/exchanges/WSExchange.kt
@@ -0,0 +1,24 @@
+package ru.exrates.entities.exchanges
+
+import ru.exrates.entities.CurrencyPair
+import ru.exrates.entities.TimePeriod
+
+class WSExchange: BasicExchange() {
+
+
+
+
+
+
+    override fun fillTop() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun currentPrice(pair: CurrencyPair, period: TimePeriod) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun priceChange(pair: CurrencyPair, interval: TimePeriod) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
